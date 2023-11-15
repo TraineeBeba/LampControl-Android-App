782c390a4f6005a58524e57e0872bb1f76816578
NorkUA <ICBazalev@gmail.com>
2023-11-15 10:14:59 +0200

    Patch0/0/30

--- app/src/main/java/com/example/myapplication/State.java
+++ app/src/main/java/com/example/myapplication/State.java
@@ -2,4 +2,8 @@ package com.example.myapplication;
 
 public abstract class State {
     public static boolean isLampOn = false;
+    public static int valueBrightnessText = (int) (3*100)/6; // 3 - middle seekBar, 6+1 - count step
+    public static int seekBarposition = 3;
+
+    public static int numberMode = 0;
 }
